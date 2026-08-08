1ff996e13ea3a9d451dc3567c4657cb7e8d0ebcb
ldoc <leandrodelolmo@gmail.com>
2026-08-08 13:40:55 +0200

    feat: create Toast notification component

---
+++ src/components/Toast.svelte
@@ -0,0 +1,18 @@
+<script>
+  let message = '';
+  let visible = false;
+  let timeout;
+
+  export function show(msg) {
+    message = msg;
+    visible = true;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      visible = false;
+    }, 2000);
+  }
+</script>
+
+<div class="toast" class:show={visible}>
+  {message}
+</div>
