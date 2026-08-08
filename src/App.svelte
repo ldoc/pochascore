36794bb2db5febfdc82c8b3f78f87c47bfdac4a0
ldoc <leandrodelolmo@gmail.com>
2026-08-08 16:36:18 +0200

    feat: update App.svelte with new navigation flow

--- src/App.svelte
+++ src/App.svelte
@@ -4,17 +4,21 @@
   import { PHASES } from './lib/constants';
   
   import WelcomeScreen from './components/WelcomeScreen.svelte';
-  import GameSetup from './components/GameSetup.svelte';
+  import GameOptions from './components/GameOptions.svelte';
+  import PlayerCount from './components/PlayerCount.svelte';
   import PlayerRegistration from './components/PlayerRegistration.svelte';
   import TablePosition from './components/TablePosition.svelte';
   import RoundSetup from './components/RoundSetup.svelte';
   import BiddingPhase from './components/BiddingPhase.svelte';
-  import PlayingPhase from './components/PlayingPhase.svelte';
-  import ScoringPhase from './components/ScoringPhase.svelte';
-  import ScoreBoard from './components/ScoreBoard.svelte';
+  import InGame from './components/InGame.svelte';
+  import ResultsEntry from './components/ResultsEntry.svelte';
+  import ScoringPhaseNew from './components/ScoringPhaseNew.svelte';
+  import FinalResults from './components/FinalResults.svelte';
+  import MuteButton from './components/MuteButton.svelte';
+  import Toast from './components/Toast.svelte';
   
   let playerCount = 4;
-  let isFlipping = false;
+  let toastComponent;
   
   onMount(() => {
     const saved = loadGame();
@@ -41,121 +45,130 @@
     saveGame($gameStore);
   }
   
-  function triggerFlip(callback) {
-    isFlipping = true;
-    setTimeout(() => {
-      callback();
-      setTimeout(() => {
-        isFlipping = false;
-      }, 50);
-    }, 300);
+  function showToast(msg) {
+    if (toastComponent) {
+      toastComponent.show(msg);
+    }
+  }
+  
+  function handleContinue() {
+    gameStore.setPhase(PHASES.SETUP);
   }
   
   function handleNewGame() {
-    triggerFlip(() => {
-      gameStore.reset();
-      gameStore.setPhase(PHASES.SETUP);
-    });
+    gameStore.reset();
+    gameStore.setPhase(PHASES.SETUP);
+    showToast('Nueva partida creada');
   }
   
   function handleResumeGame() {
     const saved = loadGame();
     if (saved) {
       gameStore.set(saved);
+      showToast('Partida cargada');
     }
   }
   
-  function handleStartRegistration(event) {
-    playerCount = event.detail.playerCount;
-    triggerFlip(() => {
-      gameStore.setPhase(PHASES.REGISTRATION);
-    });
+  function handleSelectPlayerCount(event) {
+    playerCount = event.detail.count;
+    gameStore.setPhase(PHASES.REGISTRATION);
   }
   
-  function handleRegistrationComplete() {
-    triggerFlip(() => {
-      gameStore.setPhase(PHASES.POSITIONING);
-    });
+  function handleRegistrationComplete(event) {
+    const { players } = event.detail;
+    players.forEach(player => gameStore.addPlayer(player));
+    gameStore.setPhase(PHASES.POSITIONING);
   }
   
   function handlePositioningComplete() {
-    triggerFlip(() => {
-      gameStore.setPhase(PHASES.ROUND_SETUP);
-    });
+    gameStore.setPhase(PHASES.ROUND_SETUP);
   }
   
   function handleRoundStarted() {
-    triggerFlip(() => {
-      gameStore.setPhase(PHASES.BIDDING);
-    });
+    gameStore.setPhase(PHASES.BIDDING);
   }
   
   function handleBiddingComplete() {
-    triggerFlip(() => {
-      gameStore.setPhase(PHASES.PLAYING);
-    });
+    gameStore.setPhase(PHASES.PLAYING);
   }
   
-  function handlePlayingComplete() {
-    triggerFlip(() => {
-      gameStore.setPhase(PHASES.SCORING);
-    });
+  function handleInGameComplete() {
+    gameStore.setPhase(PHASES.SCORING);
   }
   
-  function handleNextRound() {
-    triggerFlip(() => {
-      if ($gameStore.currentRound.phase === PHASES.GAME_END) {
-        gameStore.setPhase(PHASES.GAME_END);
-      } else {
-        gameStore.setPhase(PHASES.ROUND_SETUP);
-      }
-    });
+  function handleResultsComplete() {
+    gameStore.setPhase(PHASES.SCORING);
   }
   
-  function handleGameEnd() {
-    triggerFlip(() => {
+  function handleScoringNextRound() {
+    if ($gameStore.currentRound.phase === PHASES.GAME_END) {
       gameStore.setPhase(PHASES.GAME_END);
-    });
+    } else {
+      gameStore.setPhase(PHASES.ROUND_SETUP);
+    }
+  }
+  
+  function handleScoringGameEnd() {
+    gameStore.setPhase(PHASES.GAME_END);
+  }
+  
+  function handleFinalNewGame() {
+    gameStore.reset();
+    gameStore.setPhase(PHASES.WELCOME);
+    showToast('¡Hasta la próxima!');
   }
 </script>
 
-<div class="flip-container">
-  <div class="flip-panel" class:flipped={isFlipping}>
-    <main>
-      {#if phase === PHASES.WELCOME}
-        <WelcomeScreen on:newGame={handleNewGame} on:resumeGame={handleResumeGame} />
-        
-      {:else if phase === PHASES.SETUP}
-        <GameSetup on:startRegistration={handleStartRegistration} />
-        
-      {:else if phase === PHASES.REGISTRATION}
-        <PlayerRegistration totalPlayers={playerCount} on:registrationComplete={handleRegistrationComplete} />
-        
-      {:else if phase === PHASES.POSITIONING}
-        <TablePosition on:complete={handlePositioningComplete} />
-        
-      {:else if phase === PHASES.ROUND_SETUP}
-        <RoundSetup on:roundStarted={handleRoundStarted} />
-        
-      {:else if phase === PHASES.BIDDING}
-        <BiddingPhase on:biddingComplete={handleBiddingComplete} />
-        
-      {:else if phase === PHASES.PLAYING}
-        <PlayingPhase on:playingComplete={handlePlayingComplete} />
-        
-      {:else if phase === PHASES.SCORING}
-        <ScoringPhase on:nextRound={handleNextRound} on:gameEnd={handleGameEnd} />
-        
-      {:else if phase === PHASES.GAME_END}
-        <ScoreBoard on:newGame={handleNewGame} />
-      {/if}
-    </main>
-  </div>
+<div class="app-container">
+  <main>
+    {#if phase === PHASES.WELCOME}
+      <WelcomeScreen on:continue={handleContinue} />
+      
+    {:else if phase === PHASES.SETUP}
+      <GameOptions on:newGame={handleNewGame} on:continueGame={handleResumeGame} />
+      
+    {:else if phase === PHASES.REGISTRATION}
+      <PlayerCount on:select={handleSelectPlayerCount} />
+      
+    {:else if phase === PHASES.POSITIONING}
+      <PlayerRegistration totalPlayers={playerCount} on:complete={handleRegistrationComplete} />
+      
+    {:else if phase === PHASES.POSITIONING}
+      <TablePosition on:complete={handlePositioningComplete} />
+      
+    {:else if phase === PHASES.ROUND_SETUP}
+      <RoundSetup on:roundStarted={handleRoundStarted} />
+      
+    {:else if phase === PHASES.BIDDING}
+      <BiddingPhase on:biddingComplete={handleBiddingComplete} />
+      
+    {:else if phase === PHASES.PLAYING}
+      <InGame on:complete={handleInGameComplete} />
+      
+    {:else if phase === PHASES.SCORING}
+      <ResultsEntry on:complete={handleResultsComplete} />
+      
+    {:else if phase === PHASES.SCORING}
+      <ScoringPhaseNew on:nextRound={handleScoringNextRound} on:gameEnd={handleScoringGameEnd} />
+      
+    {:else if phase === PHASES.GAME_END}
+      <FinalResults on:newGame={handleFinalNewGame} />
+    {/if}
+  </main>
+  
+  <MuteButton />
+  <Toast bind:this={toastComponent} />
 </div>
 
 <style>
+  .app-container {
+    min-height: 100vh;
+    min-height: 100dvh;
+    position: relative;
+  }
+  
   main {
     min-height: 100vh;
-    background: #1e293b;
+    min-height: 100dvh;
   }
 </style>
